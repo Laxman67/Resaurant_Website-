b697a529489d4759f54f245801d5fbe2723135a7
Laxman <88457639+Laxman67@users.noreply.github.com>
2024-06-20 06:16:38 +0000

    db , cors setup done

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,10 +1,19 @@
 import express from 'express'
 import cors from 'cors'
-import { configDotenv } from 'dotenv'
-configDotenv()
-
+import dotenv from 'dotenv'
+import {dbConnection} from './database/dbConnection.js'
 
 const app = express()
+dotenv.config({path:"./config/config.env"})
 
+dbConnection()
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    methods:["POST"], // we want to post to fron end
+    credentials:'true'
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true})) // to get form data 
 
 export default app
---
+++ BACKEND/database/dbConnection.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+export const dbConnection =()=>{
+    mongoose.connect(process.env.DB_URL,{dbName:"Restaurant"
+}).then(()=>{
+    console.log("Database is Connected");
+}).catch(error=>{
+console.error(`Error Occured: ${error}`);
+})
+}
+
