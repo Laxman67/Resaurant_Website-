b494ad4de8acf0cd5aeb1cd40f4bdad968cf9ad0
Laxman <88457639+Laxman67@users.noreply.github.com>
2024-06-20 13:38:40 +0000

    general error class

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -2,18 +2,29 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import {dbConnection} from './database/dbConnection.js'
+import { errorMiddleware } from './error/error.js'
 
 const app = express()
 dotenv.config({path:"./config/config.env"})
 
-dbConnection()
+
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],
-    methods:["POST"], // we want to post to fron end
+    origin:[process.env.FRONTEND_URL], //what to connect with backend
+    methods:["POST"], // we want to post to frontend
     credentials:'true'
 }))
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true})) // to get form data 
 
+
+// Routes
+app.get("/",(req,res)=>{
+    res.json({
+        message:"Hello Welcome "
+    })
+})
+
+dbConnection()
+app.use(errorMiddleware)
 export default app
