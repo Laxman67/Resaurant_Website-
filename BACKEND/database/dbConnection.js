84f9ce7922eb645a9f520f60e655f9be278e249a
laxman67 <laxmanhenricks94@gmail.com>
2024-06-23 22:20:54 +0530

    Fixes: fractor

--- BACKEND/database/dbConnection.js
+++ BACKEND/database/dbConnection.js
@@ -1,11 +1,12 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-export const dbConnection =()=>{
-    mongoose.connect(process.env.DB_URL,{dbName:"Restaurant"
-}).then(()=>{
-    console.log("Database is Connected");
-}).catch(error=>{
-console.error(`Error Occured: ${error}`);
-})
-}
-
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.DB_URL, { dbName: 'Restaurant' })
+    .then(() => {
+      console.log('Database is Connected');
+    })
+    .catch((error) => {
+      console.error(`Error Occured: ${error}`);
+    });
+};
